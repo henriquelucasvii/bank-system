02f203c5fadfe62e2edfe05fb78688d86bc3ae82
Lucas Henrique <lucashenriquevieira86@gmail.com>
2026-05-09 23:11:00 -0300

    feat: add getter and setter on class Conta

--- src/br/com/sistemabancario2026/model/Conta.java
+++ src/br/com/sistemabancario2026/model/Conta.java
@@ -1,9 +1,40 @@
 package br.com.sistemabancario2026.model;
 
 public class Conta {
-    public int numeroAgencia;
-    public int numeroConta;
-    public String nomeCliente;
-    public double saldo;
+    private int numeroAgencia;
+    private int numeroConta;
+    private String nomeCliente;
+    private double saldo;
 
+    public int getNumeroAgencia() {
+        return numeroAgencia;
+    }
+
+    public void setNumeroAgencia(int numeroAgencia) {
+        this.numeroAgencia = numeroAgencia;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
 }
