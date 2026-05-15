dc4924af509849da696664af44e40fb2696aa531
Lucas Henrique <lucashenriquevieira86@gmail.com>
2026-05-15 13:46:04 -0300

    feat(ContaCorrenteEspecial): add new class ContaCorrenteEspecial that extends class Conta

---
+++ src/br/com/sistemabancario2026/model/ContaCorrenteEspecial.java
@@ -0,0 +1,35 @@
+package br.com.sistemabancario2026.model;
+
+public class ContaCorrenteEspecial extends Conta {
+    private float limite;
+
+    public ContaCorrenteEspecial(int numeroAgencia, int numeroConta, String nomeCliente, double saldo, float limite) {
+        super(numeroAgencia, numeroConta, nomeCliente, saldo);
+        this.limite = limite;
+    }
+
+    public float getLimite() {
+        return limite;
+    }
+
+    public void setLimite(float limite) {
+        this.limite = limite;
+    }
+
+    @Override
+    public int sacar(double valor) {
+        if (valor > (getSaldo() + this.limite)) {
+            return 0;
+        }
+
+        setSaldo(getSaldo() - valor);
+        return 1;
+    }
+
+    @Override
+    public void imprimir() {
+        super.imprimir();
+        System.out.println("Limite do Cliente: " + this.limite);
+        System.out.println("Total disponivel: " + (this.limite + this.getSaldo()));
+    }
+}
