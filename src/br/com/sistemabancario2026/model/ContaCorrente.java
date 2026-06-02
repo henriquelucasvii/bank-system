f6caf5ca301074eec802119d5575d28d9e9c7c09
Lucas Henrique <lucashenriquevieira86@gmail.com>
2026-06-02 14:05:25 -0300

    fix: fix the class names

--- src/br/com/sistemabancario2026/model/ContaCorrente.java
+++ src/br/com/sistemabancario2026/model/ContaCorrente.java
@@ -1,8 +1,8 @@
 package br.com.sistemabancario2026.model;
 
-public class Conta05Corrente extends Conta05 {
+public class ContaCorrente extends Conta {
 
-    public Conta05Corrente(int numeroAgencia, int numeroConta, String nomeCliente, double saldo) {
+    public ContaCorrente(int numeroAgencia, int numeroConta, String nomeCliente, double saldo) {
         super(numeroAgencia, numeroConta, nomeCliente, saldo);
     }
 }
